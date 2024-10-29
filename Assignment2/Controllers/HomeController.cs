7c1acd4355d653830deddc2359ca55b7de5f30bd
mickyneil-ai <gage.mcneil@oit.edu>
2024-10-28 22:05:53 -0700

    Assignment 2 done

---
+++ Assignment2/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    [Route("")]
+    public IActionResult Index()
+    {
+        ViewBag.Title = "Home";
+        return View();
+    }
+}
