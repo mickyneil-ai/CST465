168094700d4f9715fa6d37169e6b8f8338ce5d1e
MickyNeil <126832104+MickyNeil@users.noreply.github.com>
2024-11-30 14:52:20 -0800

    Add files via upload

---
+++ Lab9/Lab 9/Areas/Identity/Data/Lab_9User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Lab_9.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the Lab_9User class
+public class Lab_9User : IdentityUser
+{
+}
+
---
+++ Lab9/Lab 9/Views/_ViewStart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab_9.Views
+{
+    public class _ViewStartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
